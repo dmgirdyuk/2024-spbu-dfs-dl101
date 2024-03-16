c42d59eee7b37a5b5ff10b255e6343223a995f2a
Dmitry Girdyuk <dm.girdyuk@gmail.com>
2024-03-16 22:41:09 +0300

    updated dice loss

--- week3/train.py
+++ week3/train.py
@@ -166,15 +166,25 @@ EPSILON = 1e-7
 
 
 class MulticlassDiceLoss(nn.Module):
+    def __init__(self, eps: float = EPSILON) -> None:
+        super().__init__()
+
+        self.eps = eps
+
     def forward(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         probas = F.softmax(logits, dim=1)
 
-        intersection = (targets * probas).sum((0, 2, 3))
-        cardinality = (targets + probas).sum((0, 2, 3))
+        intersection = (targets * probas).sum((0, 2, 3)).clamp_min(self.eps)
+        cardinality = (targets + probas).sum((0, 2, 3)).clamp_min(self.eps)
 
-        dice_coefficient = (2.0 * intersection + EPSILON) / (cardinality + EPSILON)
+        dice_coefficient = (2.0 * intersection + self.eps) / (cardinality + self.eps)
 
-        return 1.0 - dice_coefficient.mean()
+        dice_loss = 1.0 - dice_coefficient
+
+        mask = targets.sum((0, 2, 3)) > 0
+        dice_loss *= mask
+
+        return dice_loss.mean()
 
 
 @dataclass
